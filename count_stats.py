326380697008b24c30e70f63a3e28f5ef6657a32
gorchakov.s <gorchakov.s@roistat.com>
2019-12-18 09:53:37 +0300

    update

--- count_stats.py
+++ count_stats.py
@@ -2,5 +2,8 @@ from CounterByThreads import CounterByThreads
 import sys
 
 if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('There must be at least one file as arg')
+        exit(1)
     counter = CounterByThreads(sys.argv[1])
     counter.get_output_file()
